7d676bc9e3c96b046190849b3f7579964fad36bd
Timofei Teterin <timofei.teterin@inbox.ru>
2022-03-26 11:07:04 +0300

    Huffman

--- AlgorithmsLibrary/HuffmanAlgm/HuffmanAlgm.cs
+++ AlgorithmsLibrary/HuffmanAlgm/HuffmanAlgm.cs
@@ -1,6 +1,150 @@
-﻿namespace AlgorithmsLibrary
+﻿using AlgorithmsLibrary.CommonClasses;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgorithmsLibrary
 {
     public static class HuffmanAlgm
     {
+        private static Dictionary<char, int> GetFrequencies(string source)
+        {
+            Dictionary<char, int> frequencies = new Dictionary<char, int>();
+
+            foreach (char c in source)
+            {
+                if (frequencies.ContainsKey(c))
+                    frequencies[c]++;
+                else
+                    frequencies.Add(c, 1);
+            }
+
+            return frequencies;
+        }
+        private static Dictionary<char, string> GetHuffmanCodes(string source)
+        {
+            var Frequencies = GetFrequencies(source);
+
+            return GetHuffmanCodes(Frequencies);
+        }
+        private static Dictionary<char, string> GetHuffmanCodes(Dictionary<char, int> frequencies)
+        {
+            //получаем словарь частот и переносим его в список
+            var nodes = frequencies.Select(x => new DoublyNode<char>(x.Key, x.Value)).ToList();
+
+            nodes.Sort(); 
+
+            //строим дерево хаффмана
+            //поскольку список узлов с частотами упорядочен, то выбираем два первых (наименьших) узла
+            //объединяем их (вес нового узла = сумме вероятностей) и добавляем обратно в список
+            //удаляя два выбранных сначала узла
+            while (nodes.Count > 1)
+            {
+                DoublyNode<char> parent = new DoublyNode<char>(nodes[0], nodes[1]);
+                nodes.RemoveRange(0, 2);
+                var index = nodes.BinarySearch(parent);
+                if (index < 0)
+                    index = ~index;
+                nodes.Insert(index, parent);
+            }
+
+            //обходим поулчившееся дерево, приписывая каждому символу свой код
+            Dictionary<char, string> codes = new Dictionary<char, string>();
+            Bypass(nodes.FirstOrDefault(), ref codes, new StringBuilder(string.Empty));
+
+            return codes;
+        }
+        //метод обхода остался еще с дискретной математики, но его кажется надо бы улучшить...
+        private static void Bypass(DoublyNode<char> Current, ref Dictionary<char, string> Codes, StringBuilder CurrentCode)
+        {
+            if (Current.Data != default)
+            {
+                Codes.Add(Current.Data, CurrentCode.ToString());
+                return;
+            }
+
+            CurrentCode.Append('0');
+            Bypass(Current.Previous, ref Codes, CurrentCode);
+            CurrentCode.Remove(CurrentCode.Length - 1, 1);
+
+            CurrentCode.Append('1');
+            Bypass(Current.Next, ref Codes, CurrentCode);
+            CurrentCode.Remove(CurrentCode.Length - 1, 1);
+        }
+
+        /// <summary>
+        /// Encoding. The algorithm compile a dictionary of frequencies from the source string, 
+        /// then builds a tree on them and gets a dictionary with Huffman codes.
+        /// </summary>
+        /// <param name="source">Source string.</param>
+        /// <returns>Encoded string.</returns>
+        public static string Encode(string source)
+        {
+            StringBuilder encoded = new StringBuilder(string.Empty);
+            Dictionary<char, string> codes = GetHuffmanCodes(source);
+
+            foreach (char c in source)
+                encoded.Append(codes[c]);
+
+            return encoded.ToString();
+        }
+
+        private static Dictionary<string, char> GetReverseCodes(Dictionary<char, string> codes)
+        {
+            return codes.ToDictionary(x => x.Value, x => x.Key);
+        }
+
+        /// <summary>
+        /// Decoding, if a frequency dictionary is available. The algorithm will first generate Huffman codes, 
+        /// then decode source string.
+        /// </summary>
+        /// <param name="frequencies">A dictionary where each character has its own frequency.</param>
+        /// <param name="encoded">Encoded string.</param>
+        /// <returns>Decoded string.</returns>
+        public static string Decode(Dictionary<char, int> frequencies, string encoded)
+        {
+            var codesForDecoding = GetHuffmanCodes(frequencies);
+
+            return Decode(codesForDecoding, encoded);
+        }
+
+        /// <summary>
+        /// Decoding, if Huffman codes are known.
+        /// </summary>
+        /// <param name="codes">A dictionary where each character corresponds to its code.</param>
+        /// <param name="encoded">Encoded string.</param>
+        /// <returns>Decoded string.</returns>
+        public static string Decode(Dictionary<char, string> codes, string encoded)
+        {
+            StringBuilder decoded = new StringBuilder(string.Empty);
+            var codesForDecoding = GetReverseCodes(codes);
+
+            string buffer = string.Empty;
+            foreach (char c in encoded)
+            {
+                buffer += c;
+                if (codesForDecoding.ContainsKey(buffer))
+                {
+                    decoded.Append(codesForDecoding[buffer]);
+                    buffer = string.Empty;
+                }
+            }
+
+            return decoded.ToString();
+        }
+
+        /// <summary>
+        /// Calculation of the compression ratio.
+        /// </summary>
+        /// <param name="sourceString">Source string.</param>
+        /// <param name="compressionString">Compression string.</param>
+        /// <returns>Compression ratio.</returns>
+        public static double CalculateCompressionRatio(string sourceString, string compressionString)
+        {
+            double countBitsSourceString = 8 * sourceString.Length;
+            double countBitsCompressionString = compressionString.Length;
+
+            return countBitsSourceString / countBitsCompressionString;
+        }
     }
 }
--- UnitTestProject/HuffmanAlgmUnitTest.cs
+++ UnitTestProject/HuffmanAlgmUnitTest.cs
@@ -1,13 +1,52 @@
 using Xunit;
+using AlgorithmsLibrary;
+using System.Collections.Generic;
 
 namespace UnitTestProject
 {
     public class HuffmanAlgmUnitTest
     {
         [Fact]
-        public void Test1()
+        public void EncodingStringAbracadabra()
         {
+            string source = "abracadabra";
 
+            string encoded = HuffmanAlgm.Encode(source);
+            string expected = "01101001110011110110100";
+            
+            Assert.Equal(expected, encoded);
+        }
+        
+        [Fact]
+        public void DecodeStringAbracadabraWithFrequencies()
+        {
+            string decoded = "01101001110011110110100";
+
+            Dictionary<char, int> frequencies = new Dictionary<char, int>
+            {
+                {'a', 5}, {'b', 2}, {'r', 2}, {'c', 1}, {'d', 1}
+            };
+
+            string encoded = HuffmanAlgm.Decode(frequencies, decoded);
+            string expected = "abracadabra";
+            
+            Assert.Equal(expected, encoded);
+        }
+        
+        [Fact]
+        public void DecodeStringAbracadabraWithCodes()
+        {
+            string decoded = "01101001110011110110100";
+
+            Dictionary<char, string> codes = new Dictionary<char, string>
+            {
+                {'a', "0"}, {'b', "110"}, {'r', "10"}, {'c', "1110"}, {'d', "1111"}
+            };
+
+            string encoded = HuffmanAlgm.Decode(codes, decoded);
+            string expected = "abracadabra";
+            
+            Assert.Equal(expected, encoded);
         }
     }
 }
