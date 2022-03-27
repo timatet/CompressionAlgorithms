e045110f9b9482153361199740cb62dc4cdbcb80
Ульяна Малеева <maleeva012002@gmail.com>
2022-03-27 23:32:26 +0300

    ArithmeticCodingAlgm ready

--- AlgorithmsLibrary/ArithmeticCodingAlgm/ArithmeticCodingAlgm.cs
+++ AlgorithmsLibrary/ArithmeticCodingAlgm/ArithmeticCodingAlgm.cs
@@ -1,7 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace AlgorithmsLibrary
 {
+    public class Symbol : IComparable<Symbol>
+    {
+        public char Data { get; set; }
+        private decimal lowRange;
+        public decimal LowRange {
+            get { return lowRange; } set { lowRange = value; HighRange = value + Frequency; } }
+        public decimal HighRange { get; private set; }
+        public decimal Frequency { get; set; }
+        public Symbol(char data, decimal frequency)
+        {
+            Data = data;
+            Frequency = frequency;
+        }
+        public Symbol(char data, decimal frequency, decimal lowRange)
+        {
+            Data = data;
+            Frequency = frequency;
+            LowRange = lowRange;
+        }
+        public int CompareTo(Symbol other)
+        {
+            return Frequency.CompareTo(other.Frequency);
+        }
+        
+    }
     public static class ArithmeticCodingAlgm
     {
         private static Dictionary<char, int> GetFrequencies(string source)
@@ -17,6 +45,67 @@ namespace AlgorithmsLibrary
             }
             return frequencies;
         }
+        private static List<Symbol> GetSymbolsRanges(string source)
+        {
+            var frequencies = GetFrequencies(source);
 
+            return GetSymbolsRanges(frequencies, source.Length);
+        }
+        private static List<Symbol> GetSymbolsRanges(Dictionary<char, int> frequencies, int CountOfAllSymbols)
+        {
+
+            decimal frequencyOfOneSymbol = (decimal)1 / CountOfAllSymbols;
+            var nodes = frequencies.Select(x => new Symbol(x.Key, x.Value * frequencyOfOneSymbol)).ToList();
+
+            nodes.Sort();
+            nodes.Reverse();
+
+            decimal current = 0;
+            foreach (var item in nodes)
+            {
+                item.LowRange = current;
+                current = item.HighRange;
+            }
+            return nodes;
+        }
+
+        public static string Encode(string source)
+        {
+            List<Symbol> codes = GetSymbolsRanges(source);
+            decimal HighRange = 1, LowRange = 0, h, l;
+            foreach (char c in source)
+            {
+                Symbol item = codes.Find(x => x.Data == c);
+                h = HighRange; l = LowRange;
+                HighRange = l + (h - l) * item.HighRange;
+                LowRange = l + (h - l) * item.LowRange;
+            }
+            decimal dec = 10;
+            decimal result = (int)(HighRange * dec) / dec;
+            while (!(result >= LowRange && result < HighRange))
+            {
+                dec *= 10;
+                result = (int)(HighRange * dec) / dec;
+            }
+            return ((int)(result*dec)).ToString();
+        }
+        public static string Decode(Dictionary<char, int> frequencies, string encoded, int CountOfAllSymbols)
+        {
+            List<Symbol> codes = GetSymbolsRanges(frequencies, CountOfAllSymbols);
+            StringBuilder decoded = new StringBuilder(string.Empty);
+
+            decimal code = int.Parse(encoded) / (decimal)Math.Pow(10, encoded.Length);
+            decimal HighRange = 1, LowRange = 0, h, l;
+            for (int i=0; i< CountOfAllSymbols; i++)
+            {
+                h = HighRange; l = LowRange;
+                Symbol item = codes.Find(x => l + (h - l) *x.HighRange>code && l + (h - l) *x.LowRange<=code);
+                decoded.Append(item.Data);
+                HighRange = l + (h - l) * item.HighRange;
+                LowRange = l + (h - l) * item.LowRange;
+            }
+
+            return decoded.ToString();
+        }
     }
 }
--- UnitTestProject/ArithmeticCodingAlgmUnitTest.cs
+++ UnitTestProject/ArithmeticCodingAlgmUnitTest.cs
@@ -1,13 +1,34 @@
-﻿using Xunit;
+﻿using AlgorithmsLibrary;
+using System.Collections.Generic;
+using Xunit;
 
 namespace UnitTestProject
 {
     public class ArithmeticCodingAlgmUnitTest
     {
         [Fact]
-        public void Test1()
+        public void EncodingStringAabcb()
         {
+            string source = "aabcb";
 
+            string encoded = ArithmeticCodingAlgm.Encode(source);
+            string expected = "125";
+
+            Assert.Equal(expected, encoded);
+        }
+        [Fact]
+        public void DecodingStringAabcb()
+        {
+            string encoded = "125";
+            Dictionary<char, int> frequencies = new Dictionary<char, int>
+            {
+                {'a', 2}, {'b', 2}, {'c', 1}
+            };
+
+            string decoded = ArithmeticCodingAlgm.Decode(frequencies, encoded, 5);
+            string expected = "aabcb";
+
+            Assert.Equal(expected, decoded);
         }
     }
 }
