d80a44967f6d1b976d6936d2a348c6e5e1cd8404
Timofei Teterin <timofei.teterin@inbox.ru>
2022-03-28 16:05:18 +0300

    refact

--- AlgorithmsLibrary/ArithmeticCodingAlgm/ArithmeticCodingAlgm.cs
+++ AlgorithmsLibrary/ArithmeticCodingAlgm/ArithmeticCodingAlgm.cs
@@ -5,31 +5,6 @@ using System.Text;
 
 namespace AlgorithmsLibrary
 {
-    public class Symbol : IComparable<Symbol>
-    {
-        public char Data { get; set; }
-        private decimal lowRange;
-        public decimal LowRange {
-            get { return lowRange; } set { lowRange = value; HighRange = value + Frequency; } }
-        public decimal HighRange { get; private set; }
-        public decimal Frequency { get; set; }
-        public Symbol(char data, decimal frequency)
-        {
-            Data = data;
-            Frequency = frequency;
-        }
-        public Symbol(char data, decimal frequency, decimal lowRange)
-        {
-            Data = data;
-            Frequency = frequency;
-            LowRange = lowRange;
-        }
-        public int CompareTo(Symbol other)
-        {
-            return Frequency.CompareTo(other.Frequency);
-        }
-        
-    }
     public static class ArithmeticCodingAlgm
     {
         private static Dictionary<char, int> GetFrequencies(string source)
---
+++ AlgorithmsLibrary/ArithmeticCodingAlgm/Symbol.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLibrary
+{
+    public class Symbol : IComparable<Symbol>
+    {
+        public char Data { get; set; }
+        private decimal lowRange;
+        public decimal LowRange
+        {
+            get { return lowRange; }
+            set { lowRange = value; HighRange = value + Frequency; }
+        }
+        public decimal HighRange { get; private set; }
+        public decimal Frequency { get; set; }
+        public Symbol(char data, decimal frequency)
+        {
+            Data = data;
+            Frequency = frequency;
+        }
+        public Symbol(char data, decimal frequency, decimal lowRange)
+        {
+            Data = data;
+            Frequency = frequency;
+            LowRange = lowRange;
+        }
+        public int CompareTo(Symbol other)
+        {
+            return Frequency.CompareTo(other.Frequency);
+        }
+
+    }
+}
