0bbfc189f1c472b3ff92b73d2cc7a6f0f6a4fd02
Ульяна Малеева <maleeva012002@gmail.com>
2022-04-02 00:42:27 +0300

    part of the border for Huffman, Fano and arifmatic

--- AlgorithmsWpf/MainWindow.xaml.cs
+++ AlgorithmsWpf/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace AlgorithmsWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        // создать булевскй массив для определения алгоритма
         public MainWindow()
         {
             InitializeComponent();
@@ -27,37 +28,94 @@ namespace AlgorithmsWpf
 
         private void HuffmanClicButton(object sender, RoutedEventArgs e)
         {
+            //все доски сделать невидимыми
+
+            //Очистить поля нужного алгоритма
+            // поменять название
+            // сделать видимой только одну доску
 
         }
 
         private void ShannonFanoClicButton(object sender, RoutedEventArgs e)
         {
+            //все доски сделать невидимыми
+
+            //Очистить поля нужного алгоритма
+            // сделать видимой только одну доску
 
         }
 
         private void ArithmeticCodingClicButton(object sender, RoutedEventArgs e)
         {
+            //все доски сделать невидимыми
+
+            //Очистить поля нужного алгоритма
+            // сделать видимой только одну доску
 
         }
 
         private void RLEClicButton(object sender, RoutedEventArgs e)
         {
+            //все доски сделать невидимыми
+
+            //Очистить поля нужного алгоритма
+            // сделать видимой только одну доску
 
         }
 
         private void LZ77ClicButton(object sender, RoutedEventArgs e)
         {
+            //все доски сделать невидимыми
+
+            //Очистить поля нужного алгоритма
+            // сделать видимой только одну доску
 
         }
 
         private void HammingClicButton(object sender, RoutedEventArgs e)
         {
+            //все доски сделать невидимыми
+
+            //Очистить поля нужного алгоритма
+            // сделать видимой только одну доску
 
         }
 
         private void LinearCodesType52ClicButton(object sender, RoutedEventArgs e)
         {
+            //все доски сделать невидимыми
 
+            //Очистить поля нужного алгоритма
+            // сделать видимой только одну доску
+
+        }
+
+        private void EncodeClicButton(object sender, RoutedEventArgs e)
+        {
+            // в зависимости от алгоритма запустить кодировку
+            // проверка на пустоту поля для исходого текста
+        }
+
+        private void DecodeClicButton(object sender, RoutedEventArgs e)
+        {
+
+            // в зависимости от алгоритма запустить декодировку
+            // проверка на пустоту полей для исходого текста
+        }
+
+        private void EncodeFromFileClicButton(object sender, RoutedEventArgs e)
+        {
+            // диалоговое окно
+        }
+
+        private void DecodeFromFileClicButton(object sender, RoutedEventArgs e)
+        {
+            //диалоговое окно
+        }
+
+        private void ClearAllClicButton(object sender, RoutedEventArgs e)
+        {
+            //в зависимости от алгоритма очистить поля
         }
     }
 }
