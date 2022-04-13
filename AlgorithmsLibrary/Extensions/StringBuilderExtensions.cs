59d289e4c78c4b052553f546a7b0a3c44a98df34
Timofei Teterin <timofei.teterin@inbox.ru>
2022-04-13 13:07:45 +0300

    modified SB Ext & new icon

--- AlgorithmsLibrary/Extensions/StringBuilderExtensions.cs
+++ AlgorithmsLibrary/Extensions/StringBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace AlgorithmsLibrary.StringBuilderExtensions
 {
@@ -14,12 +15,63 @@ namespace AlgorithmsLibrary.StringBuilderExtensions
         }
         public static string Substring(this StringBuilder @this, int startpos, int length)
         {
-            return @this.ToString().Substring(startpos, length);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = startpos; i < length; i++)
+            {
+                stringBuilder.Append(@this[i]);
+            }
+            return stringBuilder.ToString();
+                //@this.ToString().Substring(startpos, length);
         }
 
-        public static int IndexOf(this StringBuilder @this, string match)
+        public static int IndexOf(this StringBuilder haystack, string needle)
         {
-            return @this.ToString().IndexOf(match);
+            if (haystack == null || needle == null)
+                throw new ArgumentNullException();
+            if (needle.Length == 0)
+                return 0;//empty strings are everywhere!
+            if (needle.Length == 1)//can't beat just spinning through for it
+            {
+                char c = needle[0];
+                for (int idx = 0; idx != haystack.Length; ++idx)
+                    if (haystack[idx] == c)
+                        return idx;
+                return -1;
+            }
+            int m = 0;
+            int i = 0;
+            int[] T = KMPTable(needle);
+            while (m + i < haystack.Length)
+            {
+                if (needle[i] == haystack[m + i])
+                {
+                    if (i == needle.Length - 1)
+                        return m == needle.Length ? -1 : m;//match -1 = failure to find conventional in .NET
+                    ++i;
+                }
+                else
+                {
+                    m = m + i - T[i];
+                    i = T[i] > -1 ? T[i] : 0;
+                }
+            }
+            return -1;
+        }
+        private static int[] KMPTable(string sought)
+        {
+            int[] table = new int[sought.Length];
+            int pos = 2;
+            int cnd = 0;
+            table[0] = -1;
+            table[1] = 0;
+            while (pos < table.Length)
+                if (sought[pos - 1] == sought[cnd])
+                    table[pos++] = ++cnd;
+                else if (cnd > 0)
+                    cnd = table[cnd];
+                else
+                    table[pos++] = 0;
+            return table;
         }
     }
 }
