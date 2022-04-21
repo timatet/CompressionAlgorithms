13c9053c54def7e3f1d47b787520a7e91862ca78
Timofei Teterin <timofei.teterin@inbox.ru>
2022-04-21 17:03:48 +0300

    add int exstension

---
+++ AlgorithmsLibrary/Extensions/IntExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace AlgorithmsLibrary.IntExtensions
+{
+    public static class IntExtensions
+    {
+        public static bool InRange(this int @this, int left, int right)
+        {
+            if (@this >= left && @this <= right)
+                return true;
+            return false;
+        }
+        public static bool InRange(this decimal @this, decimal left, decimal right)
+        {
+            if (@this >= left && @this <= right)
+                return true;
+            return false;
+        }
+        public static bool InRange(this double @this, double left, double right)
+        {
+            if (@this >= left && @this <= right)
+                return true;
+            return false;
+        }
+        public static bool InRange(this long @this, long left, long right)
+        {
+            if (@this >= left && @this <= right)
+                return true;
+            return false;
+        }
+    }
+}
